b0b410237623c1b5ec4687cb76f5a0d8e7334dcb
Olivier <o_ribiere@cartong.org>
2018-10-31 14:57:02 +0100

    add changes

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ var CartONG = {};
 CartONG.config = {};
 
 CartONG.config.content = {
-  header: 'GeOnG 2018 test',
+  header: 'GeOnG 2018 test local',
   subHeader: 'Github Workshop',
   description: 'Get started with Github',
   source: 'https://photon.komoot.de/'
